2e54251bddfbb6583e0fe891d10b6019cbd63560
kaljitism <dwikaljit@gmail.com>
2024-09-04 17:17:16 +0530

    application entrypoint

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Serenity',
+      theme: ThemeData.dark(
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Missions'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Text(
+          'Hello, World',
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Add Mission',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
